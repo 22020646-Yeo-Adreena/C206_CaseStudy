e9ef151e833ea3d0db0ad4bc766e9ec2181c092e
22020646 <22020646@LAPTOP-38E70BF0>
2023-08-16 10:59:39 +0800

    service provider codes

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,9 +1,121 @@
+import java.util.ArrayList;
 
 public class C206_CaseStudy {
 
 	public static void main(String[] args) {
+
+		ArrayList<ServiceProvider> serviceProvider = new ArrayList<ServiceProvider>();
 		// TODO Auto-generated method stub
+		ServiceProvider provider1 = new ServiceProvider("user1", "user1@example.com", "Allbest Marine", "Renovator");
+		ServiceProvider provider2 = new ServiceProvider("user2", "user2@example.com", "Allbest Equipment", "Designer");
+
+		serviceProvider.add(provider1);
+		serviceProvider.add(provider2);
+
+		int option = 0;
+
+		while (option != 4) {
+			C206_CaseStudy.menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				// Add service provider
+				ServiceProvider newProvider = inputProvider();
+				
+				for(ServiceProvider provider : serviceProvider) {
+					if(newProvider.equals(serviceProvider)) {
+						System.out.println("There is an existing service provider");
+					}else {
+						C206_CaseStudy.addServiceProvider(serviceProvider, newProvider);
+					}
+				}
+				
+			} else if (option == 2) {
+				// View all service providers
+				C206_CaseStudy.viewServiceProviders(serviceProvider);
+			} else if (option == 3) {
+				//Display list of service providers to choose from
+				C206_CaseStudy.viewServiceProviders(serviceProvider);
+				// Delete an existing service provider
+				C206_CaseStudy.deleteServiceProvider(serviceProvider);
+			} else {
+				System.out.println("Goodbye!");
+			}
+
+		}
 
 	}
 
+	public static void menu() {
+		System.out.println("RENOVATION PORTAL");
+		System.out.println("1. Add service provider");
+		System.out.println("2. View all service provider");
+		System.out.println("3. Delete an existing service provider");
+		System.out.println("4. Quit");
+		Helper.line(80, "-");
+
+	}
+
+	public static ServiceProvider inputProvider() {
+		String username = Helper.readString("Enter username >");
+		String email = Helper.readString("Enter email >");
+		String company = Helper.readString("Enter company >");
+		String credentials = Helper.readString("Enter credentials >");
+		
+
+		ServiceProvider newProvider = new ServiceProvider(username, email, company, credentials);
+		return newProvider;
+
+	}
+
+	public static void addServiceProvider(ArrayList<ServiceProvider> serviceProvider, ServiceProvider newProvider) {
+		serviceProvider.add(newProvider);
+	}
+	public static String retrieveAllServiceProvider(ArrayList<ServiceProvider> serviceProvider) {
+		String output = "";
+		
+		for (ServiceProvider provider : serviceProvider) {
+			output += String.format("%s %-10s %-15s %-10s\n", provider.getUsername(),
+					 provider.getEmail(), provider.getCompany(), provider.getCredentials());
+		}
+		return output;
+		
+	}
+
+	public static void viewServiceProviders(ArrayList<ServiceProvider> serviceProvider) {
+		System.out.println("SERVICE PROVIDER LIST");
+		String output = String.format("%s %-10s %-15s %-10s", "Username",
+				"Email", "Company", "Credentials");
+		output += retrieveAllServiceProvider(serviceProvider);
+		System.out.println(output);
+	}
+	public static boolean confirmDeleteServiceProvider(ArrayList<ServiceProvider> serviceProvider, String username, char confirm) {
+		for(int i =0; i < serviceProvider.size(); i++) {
+			ServiceProvider provider = serviceProvider.get(i);
+			 if (provider.getUsername().equalsIgnoreCase(username)) {
+	               if(confirm == 'y' || confirm == 'Y') {
+	                serviceProvider.remove(provider);
+	                return true;
+	               }
+	              }
+	          }
+	          return false;
+		}
+		
+	
+
+	public static void deleteServiceProvider(ArrayList<ServiceProvider> serviceProvider) {
+		String username = Helper.readString("\nEnter Username to delete: ");
+	    char confirm = Helper.readChar("Are you sure do you want to delete (y/n): ");
+	          boolean isDeleted = confirmDeleteServiceProvider(serviceProvider, username, confirm);
+	          
+	          if (isDeleted) {
+	           System.out.println("\n* User has been deleted successfully. *\n");
+	          }else if(confirm == 'n' || confirm == 'N') {
+	           System.out.println("\n* Deletion cancel! *\n");
+	          }else {
+	              System.out.println("\n* Invalid User! *\n");
+	          }
+		
+	}
 }
--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,23 +1,124 @@
 import static org.junit.Assert.*;
-
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
 public class C206_CaseStudyTest {
-
+	//prepare test data
+	private ServiceProvider provider1;
+	private ServiceProvider provider2;
+	
+	private static ArrayList<ServiceProvider> serviceProvider;
+	
+	public C206_CaseStudyTest() {
+		super();
+	}
 	@Before
 	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
+		provider1 = new ServiceProvider("user1", "user1@example.com", "Allbest Marine", "Renovator");
+		provider2 = new ServiceProvider("user2", "user2@example.com", "Allbest Equipment", "Designer");
+		
+		serviceProvider = new ArrayList<ServiceProvider>();
 	}
 
 	@Test
+	public void testAddServiceProvider() {
+		// Adding a new service provider
+		// Given an empty list, after adding one service provider, the size of the list
+		// is 1- normal
+		C206_CaseStudy.addServiceProvider(serviceProvider, provider1);
+		assertEquals("Test that the Service Provider arraylist size is 1.", 1, serviceProvider.size());
+		// Adding multiple service providers -boundary
+		C206_CaseStudy.addServiceProvider(serviceProvider, provider2);
+		assertEquals("Test that the Service Provider arraylist size is 2.", 2, serviceProvider.size());
+		// checking if the list size is 2
+		assertEquals(2, serviceProvider.size());
+		// Adding duplicate service providers -error
+		C206_CaseStudy.addServiceProvider(serviceProvider, provider1);
+		assertSame("Test that the service provider added is as same as the previous item added", provider1,
+				serviceProvider.get(0));
+
+	}
+	@Test
+	public void testRetrieveAllServiceProvider() {
+		// Test Case 1
+		// Test if Item list is not null and empty-boundary
+		assertNotNull("Test if there is valid service provider arraylist to add to", serviceProvider);
+		assertEquals("Test that the serviceProvider arraylist is empty.", 0, serviceProvider.size());
+		// Attempt to retrieve the service providers
+		String allServiceProviders = C206_CaseStudy.retrieveAllServiceProvider(serviceProvider);
+		String testOutput = "";
+		// Test if the output is empty
+		assertEquals("Test that nothing is displayed", testOutput, allServiceProviders);
+		// Test Case 2
+		// Test if can retrieve service providers-normal
+		C206_CaseStudy.addServiceProvider(serviceProvider, provider1);
+		C206_CaseStudy.addServiceProvider(serviceProvider, provider2);
+		// Test that the list is not empty
+		assertEquals("Test that ServiceProvider arraylist size is 2.", 2, serviceProvider.size());
+		// Attempt to retrieve the service providers
+		allServiceProviders = C206_CaseStudy.retrieveAllServiceProvider(serviceProvider);
+		testOutput = String.format("%s %-10s %-15s %-10s\n", "user1", "user1@example.com", "Allbest Marine", "Renovator");
+		testOutput += String.format("%s %-10s %-15s %-10s\n", "user2", "user2@example.com", "Allbest Equipment", "Designer");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allServiceProviders);
+		// Test Case 3
+		// Test can view service providers with updated list- normal
+		// Add new provider
+		ServiceProvider newProvider = new ServiceProvider("user3", "user3@gmail.com", "Allbest delivery", "Constructor");
+		C206_CaseStudy.addServiceProvider(serviceProvider, newProvider);
+		// Attempt to retrieve the service providers
+		allServiceProviders = C206_CaseStudy.retrieveAllServiceProvider(serviceProvider);
+		testOutput = String.format("%s %-10s %-15s %-10s\n", "user1", "user1@example.com", "Allbest Marine", "Renovator");
+		testOutput += String.format("%s %-10s %-15s %-10s\n", "user2", "user2@example.com", "Allbest Equipment", "Designer");
+		testOutput += String.format("%s %-10s %-15s %-10s\n", "user3", "user3@gmail.com", "Allbest delivery", "Constructor");
+		// Test that the details are displayed correctly
+		assertEquals("Test that the display is correct.", testOutput, allServiceProviders);
+
+	}
+	
+	@Test
+    public void testDeleteServiceProvider() {
+     assertNotNull("Check if there is valid service provider arraylist to delete from", serviceProvider);
+
+     // Adding service providers for testing
+     C206_CaseStudy.addServiceProvider(serviceProvider, provider1);
+     C206_CaseStudy.addServiceProvider(serviceProvider, provider2);
+
+     // Test delete a service provider
+     String deletedUsername = provider1.getUsername();
+     boolean isDeleted = C206_CaseStudy.confirmDeleteServiceProvider(serviceProvider, deletedUsername, 'y');
+     assertEquals("Test that user1 is deleted", true, isDeleted);
+     assertEquals("Test that user arraylist size is 1", 1, serviceProvider.size());
+     assertSame("Test that user2 is still in the list", provider2, serviceProvider.get(0));
+
+     // Test to cancel deletion 
+     String undeletedUsername = provider2.getUsername();
+     isDeleted = C206_CaseStudy.confirmDeleteServiceProvider(serviceProvider, undeletedUsername, 'n');
+     assertEquals("Test that user2 is not deleted", false, isDeleted);
+     assertEquals("Test that user arraylist size is 1", 1, serviceProvider.size());
+     assertSame("Test that user2 is still in the list", provider2, serviceProvider.get(0));
+
+     // Test deleting a service provider thats not in the list
+     String nonexistentServiceProvider = "nonexistentServiceProvider";
+     isDeleted = C206_CaseStudy.confirmDeleteServiceProvider(serviceProvider, nonexistentServiceProvider, 'y');
+     assertEquals("Test that nonexistent user is not deleted", false, isDeleted);
+     assertEquals("Test that user arraylist size is still 1", 1, serviceProvider.size());
+     assertSame("Test that user2 is still in the list", provider2, serviceProvider.get(0));
+     
+    }
+	
+	@After
+	public void tearDown() throws Exception {
+		provider1 = null;
+		provider2 = null;
+		serviceProvider = null;
+	}
+	@Test
 	public void c206_test() {
-		//fail("Not yet implemented"); 
+		//fail("Not yet implemented");
 		assertTrue("C206_CaseStudy_SampleTest ",true);
 	}
-
+	
 }
+
